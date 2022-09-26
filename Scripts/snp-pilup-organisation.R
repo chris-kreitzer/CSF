00df663b753b8dbf7fb4c0eb840a7eaca8bb7bc8
chris kreitzer <christoph.kreitzer93@gmx.at>
2022-09-26 18:34:31 +0200

    organisation out

--- Scripts/snp-pilup-organisation.R
+++ Scripts/snp-pilup-organisation.R
@@ -74,7 +74,10 @@ all_out = all_out[!with(all_out, is.na(sample) & is.na(path)), ]
 all_out = all_out[!with(all_out, duplicated(sample) & duplicated(path)), ]
 CSF_out = all_out %>% distinct(sample, path, .keep_all = TRUE)
 
-
+##-----------------
+## Research samples 
+## with no matched normal
+##-----------------
 nu = data.frame()
 for(i in unique(all_out$PATIENT_ID)){
   n = length(unique(all_out$sample[which(all_out$PATIENT_ID == i)]))
@@ -101,31 +104,78 @@ m.normal = m.normal[!m.normal == 'NA']
 ## SAMPLE pairing if 
 ## no DMP is available
 ##-----------------
-ID_match = data.frame()
-for(i in unique(CSF_out$PATIENT_ID)){
-  if(any(grepl(pattern = 'P-0', CSF_out$sample[which(CSF_out$PATIENT_ID == i)]))) {
-    out = CSF_out[which(CSF_out$PATIENT_ID == i), ]
-  }
-  else {
-    CSF_sub = CSF_out[which(CSF_out$PATIENT_ID == i), ]
-    CSF_sample = as.character(CSF_sub[1, 'sample'])
-    normal_csf = sample_pairing$V1[grepl(pattern = CSF_sample, sample_pairing$V2)]
-    normal_csf = ifelse(length(normal_csf) > 1, normal_csf[1], normal_csf)
-    normal_csf_path = grep(pattern = normal_csf, x = research$V2, value = T)
-    normal_csf_path = grep(pattern = '.bam$', x = normal_csf_path, value = T)
-    normal_csf_path = paste0(research_path, substring(normal_csf_path, first = 3))
-    
-    out = rbind(CSF_sub, data.frame(PATIENT_ID = i,
-                                    sample = normal_csf,
-                                    path = normal_csf_path))
-  }
-  ID_match = rbind(ID_match, out)
-  rm(out)
+m.normal
+missing = data.frame()
+for(i in unique(m.normal)){
+  try({
+    if(any(grepl(pattern = substr(x = i, start = 3, stop = nchar(i)), x = sample_pairing$V2))){
+      matched = sample_pairing$V1[grepl(pattern = substr(x = i, start = 3, stop = nchar(i)), x = sample_pairing$V2)]
+      matched = ifelse(length(matched) > 1, matched[1], matched)
+      # matched_path = grep(pattern = matched, research$V2, value = T)
+      # matched_path = ifelse(length(matched_path) > 1, matched_path[1], matched_path)
+      # matched_path = grep(pattern = '.bam$', x = matched_path, value = T)
+      out = data.frame(PATIENT_ID = i,
+                       sample = matched,
+                       path = NA)
+      missing = rbind(missing, out)
+      rm(matched, out)
+    }
+  })
 }
 
-ID_match = ID_match %>% distinct(sample, .keep_all = TRUE)
+for(i in 1:nrow(missing)){
+  if(any(grepl(pattern = missing$sample[i], research$V2))){
+    path = grep(pattern = missing$sample[i], research$V2, value = T)
+    path = grep(pattern = '.bam$', x = path, value = T)
+    path = ifelse(length(path) > 1, path[1], path)
+    missing$path[i] = path
+  }
+}
 
-write.table(x = ID_match, file = '~/Documents/MSKCC/Subhi/CSF/Data/FINAL_samples/sample_match.txt', sep = '\t', row.names = F, quote = F)
+
+##-----------------
+CSF_match = rbind(all_out, missing)
+CSF_match$path = ifelse(grepl(pattern = '^\\.', CSF_match$path),
+                        paste0(research_path, substr(CSF_match$path, start = 3, stop = nchar(CSF_match$path))), 
+                        CSF_match$path)
+
+
+write.table(x = CSF_match, file = '~/Documents/MSKCC/Subhi/CSF/Data/FINAL_samples/sample_match.txt', sep = '\t', row.names = F, quote = F)
+
+
+
+
+
+
+##-----------------
+#' out
+
+
+# ID_match = data.frame()
+# for(i in unique(CSF_out$PATIENT_ID)){
+#   if(any(grepl(pattern = 'P-0', CSF_out$sample[which(CSF_out$PATIENT_ID == i)]))) {
+#     out = CSF_out[which(CSF_out$PATIENT_ID == i), ]
+#   }
+#   else {
+#     CSF_sub = CSF_out[which(CSF_out$PATIENT_ID == i), ]
+#     CSF_sample = as.character(CSF_sub[1, 'sample'])
+#     normal_csf = sample_pairing$V1[grepl(pattern = CSF_sample, sample_pairing$V2)]
+#     normal_csf = ifelse(length(normal_csf) > 1, normal_csf[1], normal_csf)
+#     normal_csf_path = grep(pattern = normal_csf, x = research$V2, value = T)
+#     normal_csf_path = grep(pattern = '.bam$', x = normal_csf_path, value = T)
+#     normal_csf_path = paste0(research_path, substring(normal_csf_path, first = 3))
+#     
+#     out = rbind(CSF_sub, data.frame(PATIENT_ID = i,
+#                                     sample = normal_csf,
+#                                     path = normal_csf_path))
+#   }
+#   ID_match = rbind(ID_match, out)
+#   rm(out)
+# }
+# 
+# ID_match = ID_match %>% distinct(sample, .keep_all = TRUE)
+# 
+# write.table(x = ID_match, file = '~/Documents/MSKCC/Subhi/CSF/Data/FINAL_samples/sample_match.txt', sep = '\t', row.names = F, quote = F)
 
 #' out
 
