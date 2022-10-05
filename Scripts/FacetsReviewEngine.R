725e09e2c39bc2a429768f00533f6fe37c68e588
chris kreitzer <christoph.kreitzer93@gmx.at>
2022-10-05 07:08:52 +0200

    update

--- Scripts/FacetsReviewEngine.R
+++ Scripts/FacetsReviewEngine.R
@@ -16,11 +16,11 @@ sample_match = read.csv('Data/FINAL_samples/sample_match.txt', sep = '\t')
 sample_pairing = read.csv('Data/FINAL_samples/sample_match.txt', sep = '\t')
 
 ## C-000597: countMatrix
-countMatrix_path = 'C-04MK4H/C-04MK4H__countMatrix.dat.gz'
+countMatrix_path = 'C-05TMCR/C-05TMCR__countMatrix.dat.gz'
 countMatrix_raw = read.csv(file = countMatrix_path, sep = ',')
 samples = grep(pattern = 'File*', colnames(countMatrix_raw))
 samples = (length(samples) - 4) / 4
-ID = 'C-04MK4H'
+ID = 'C-05TMCR'
 snp_pileup[which(snp_pileup$Patient_ID == ID), ]
 
 ## Parameters: (exclusively purity runs); not interested in gene_level alterations
@@ -201,7 +201,7 @@ fit = facetsSuite::run_facets(read_counts = manual,
                               cval = cval,
                               min_nhet = min_het,
                               seed = seed,
-                              genome = 'hg19', 0.06)
+                              genome = 'hg19')
 fit$dipLogR
 i = facetsSuite::cnlr_plot(fit, return_object = T)
 ii = facetsSuite::valor_plot(fit, return_object = T)
@@ -213,7 +213,7 @@ j = facets_fit_qc(fit)
 j
 
 
-samples_dipLogR = c(0.06)
+samples_dipLogR = c(0.08130705)
 
 
 ##-----------------
