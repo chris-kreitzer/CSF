6a0d54e33599c0758861dd5779b91047972d6eb8
chris kreitzer <christoph.kreitzer93@gmx.at>
2022-10-20 12:16:16 +0200

    out

--- Scripts/FacetsReviewEngine.R
+++ Scripts/FacetsReviewEngine.R
@@ -16,11 +16,11 @@ sample_match = read.csv('Data/FINAL_samples/sample_match.txt', sep = '\t')
 sample_pairing = read.csv('Data/FINAL_samples/sample_pairing.txt', sep = '\t')
 
 ## C-000597: countMatrix
-countMatrix_path = 'C-000624/C-000624__countMatrix_new.dat.gz'
+countMatrix_path = 'C-006887/C-006887__countMatrixNew.dat.gz'
 countMatrix_raw = read.csv(file = countMatrix_path, sep = ',')
 samples = grep(pattern = 'File*', colnames(countMatrix_raw))
 samples = (length(samples) - 4) / 4
-ID = 'C-000624'
+ID = 'C-006887'
 snp_pileup[which(snp_pileup$Patient_ID == ID), ]
 
 ## Parameters: (exclusively purity runs); not interested in gene_level alterations
