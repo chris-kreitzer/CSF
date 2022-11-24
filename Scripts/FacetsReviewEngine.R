40926f9466569415c1362f9a5875055b04689c09
chris kreitzer <christoph.kreitzer93@gmx.at>
2022-11-24 21:00:50 +0100

    Engine update

--- Scripts/FacetsReviewEngine.R
+++ Scripts/FacetsReviewEngine.R
@@ -16,11 +16,11 @@ sample_match = read.csv('Data/FINAL_samples/sample_match.txt', sep = '\t')
 sample_pairing = read.csv('Data/FINAL_samples/sample_pairing.txt', sep = '\t')
 
 ## C-000597: countMatrix
-countMatrix_path = 'C-Y8JT3D/C-Y8JT3D__countMatrixNew.dat.gz'
+countMatrix_path = 'C-001796/C-001796__countMatrix.dat.gz'
 countMatrix_raw = read.csv(file = countMatrix_path, sep = ',')
 samples = grep(pattern = 'File*', colnames(countMatrix_raw))
 samples = (length(samples) - 4) / 4
-ID = 'C-Y8JT3D'
+ID = 'C-001796'
 snp_pileup[which(snp_pileup$Patient_ID == ID), ]
 
 ## Parameters: (exclusively purity runs); not interested in gene_level alterations
@@ -202,7 +202,7 @@ for(tumor_sample in 1:nrow(parameter_table)){
 ##-----------------
 ## manual inspection and re-run
 ##-----------------
-manual = multi_readSnpMatrix(filename = countMatrix_path, tumor_sample = 2)
+manual = multi_readSnpMatrix(filename = countMatrix_path, tumor_sample = 3)
 fit = facetsSuite::run_facets(read_counts = manual, 
                               cval = cval,
                               min_nhet = min_het,
