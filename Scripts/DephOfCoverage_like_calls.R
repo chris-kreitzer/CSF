5653c72934a7ab752a2f85da6bc1aba80f212934
chris kreitzer <christoph.kreitzer93@gmx.at>
2023-03-29 20:08:50 +0200

    update

--- Scripts/DephOfCoverage_like_calls.R
+++ Scripts/DephOfCoverage_like_calls.R
@@ -20,37 +20,62 @@ for(i in unique(CSF_samples$Sample.ID)){
   
   diplogr = datain$dipLogR
   segs = datain$segs
+  snps = datain$snps
   
-  gene = facetsSuite::gene_level_changes(facets_output = datain, genome = 'hg19', algorithm = 'em')
-  gene = gene[which(gene$gene %in% GOIs), c('gene', 'chrom', 'seg', "median_cnlr_seg", 'cn_state')]
+  ##-- Assume that all the CnLR are normally distributed
+  Mean = mean(snps$cnlr, na.rm = T)
+  Sd = sd(snps$cnlr, na.rm = T)
   
-  segs$adj = ifelse(segs$cnlr.median.clust < 0, segs$cnlr.median.clust - diplogr,
-                    segs$cnlr.median.clust - diplogr)
-  segs = segs[,c('chrom', 'seg', 'cnlr.median.clust', 'adj')]
+  x = seq(floor(min(snps$cnlr)), ceiling(max(snps$cnlr)), 0.01)
+  xp = pnorm(x, mean = Mean, sd = Sd)
   
-  gene_new = merge(gene, segs, by = c('chrom', 'seg'), all.x = T)
+  ##-- lower threshold 5%
+  lower = x[max(which(xp <= 0.1, arr.ind = T))]
+  upper = x[min(which(xp >= 0.9, arr.ind = T))]
   
-  cnlr_distribution = quantile(segs$adj, probs = c(0.15, 0.25, 0.75, 0.85))
+  # plot(x, pnorm(x, mean = Mean, sd = Sd), type = "l",
+  #      ylim = c(0, 1), ylab = "P(X < x)", lwd = 2, col = "red")
+  # 
+  # ##-- lower bound
+  # abline(v = x[max(which(xp < 0.05, arr.ind = T))])
+  # ##-- upper bound
+  # abline(v = x[min(which(xp >= 0.95, arr.ind = T))])
   
-  gene_new$call = ifelse(gene_new$adj <= cnlr_distribution[[1]], '-2',
-                         ifelse(gene_new$adj >= cnlr_distribution[[4]], '2', 0))
   
-  gene_new = gene_new[,c('chrom', 'gene', 'adj', 'call')]
-  colnames(gene_new)[3] = 'adjusted_cnlr'
-  gene_new$dipLogR = diplogr
-  gene_new = gene_new[,c(2,1,3,5,4)]
-  gene_new$id = i
+  ##-- CDKN2A CnLR positions
+  cdkn2a = genes_hg19[which(genes_hg19$gene == 'CDKN2A'), ]
+  cdkn2a_snps = snps[which(snps$chrom == cdkn2a$chrom &
+                             snps$maploc >= cdkn2a$start & 
+                             snps$maploc <= cdkn2a$end), ]
+  cdkn2a_mean = mean(cdkn2a_snps$cnlr)
   
-  csf_out = rbind(csf_out, gene_new)
+  ##-- EGFR CnLR positions
+  egfr = genes_hg19[which(genes_hg19$gene == 'EGFR'), ]
+  egfr_snps = snps[which(snps$chrom == egfr$chrom &
+                             snps$maploc >= egfr$start & 
+                             snps$maploc <= egfr$end), ]
+  egfr_mean = mean(egfr_snps$cnlr)
   
-  rm(segs, gene, datain, file, gene_new, diplogr, cnlr_distribution)
+  out = data.frame(id = i,
+                   gene = c('CDKN2A', 'EGFR'),
+                   mean_value = c(cdkn2a_mean, egfr_mean),
+                   n_loci = c(nrow(cdkn2a_snps), nrow(egfr_snps)),
+                   lower = lower,
+                   upper = upper,
+                   call = c(ifelse(cdkn2a_mean <= lower, '-2', 0),
+                            ifelse(egfr_mean >= upper, '2', 0)))
   
+  csf_out = rbind(csf_out, out)
+  rm(segs, snps, datain, file, egfr, cdkn2a, diplogr, lower, upper, x, xp, Mean, Sd)
 }
-
+  
+ 
 length(csf_out$id[which(csf_out$gene == 'CDKN2A' & csf_out$call == '-2')]) / length(csf_out$gene[which(csf_out$gene == 'CDKN2A')])
 ## 24% CDKN2A
 length(csf_out$id[which(csf_out$gene == 'EGFR' & csf_out$call == '2')]) / length(csf_out$gene[which(csf_out$gene == 'EGFR')])
 ## 45% EGFR
+
+
 
 
 ##-- TUMOR
